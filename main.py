aae82f15517f93b1298df0949fccca8b18db3504
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2023-01-28 14:58:25 +0000

    upfdate apps

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -12,15 +15,39 @@ class Item(db.Model):
   price = db.Column(db.Integer, nullable = False)
   isActive = db.Column(db.Boolean, default=True)
   # text = db.Column(db.Text, nullable = False)
+  def __repr__(self):
+    return f'ieraksts: {self.title}'
+
+
+
+
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  items = Item.query.order_by(Item.price).all()
+  return render_template("index.html", data=items)
 
 @app.route('/about')
 def about():
   return render_template("about.html")
 
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+  if request.method == "POST":
+    title = request.form['title']
+    price = request.form['price']
 
+    item = Item(title=title, proce=price)
+
+    try:
+      db.session.add(item)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return "Kļūda!"
+  else:
+    return render_template("create.html")
+
+  
 # if __name__ =="__maim__":
 app.run(host='0.0.0.0', port=8080)
