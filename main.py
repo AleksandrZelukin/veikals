e67cce0af27eb266dd050aa34334beb02c7103b6
Aleksandr <alexandr44@inbox.lv>
2023-01-28 21:17:27 +0200

    update apps

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ class Item(db.Model):
   title = db.Column(db.String(100), nullable = False)
   price = db.Column(db.Integer, nullable = False)
   isActive = db.Column(db.Boolean, default=True)
-  # text = db.Column(db.Text, nullable = False)
+  text = db.Column(db.Text, nullable = False)
+
+
   def __repr__(self):
-    return f'ieraksts: {self.title}'
+    return self.title
 
 @app.route('/')
 def index():
-  items = Item.query.order_by(Item.price).all()
+  items = Item.query.order_by(Item.id).all()
   return render_template("index.html", data=items)
 
 @app.route('/about')
@@ -29,8 +31,9 @@ def create():
   if request.method == "POST":
     title = request.form['title']
     price = request.form['price']
+    text = request.form['text']
 
-    item = Item(title=title, price=price)
+    item = Item(title=title, price=price, text=text)
 
     try:
       db.session.add(item)
@@ -41,5 +44,5 @@ def create():
   else:
     return render_template("create.html")
   
-# if __name__ =="__maim__":
-app.run(host='0.0.0.0', port=8081)
+if __name__ =="__main__":
+  app.run(host='0.0.0.0', port=8081)
