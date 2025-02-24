0c5cd35464e905fe802e5f9df2911ad3865dbeca
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-02-24 11:57:26 +0200

    Update apps

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+# db = SQLAlchemy(app)
 
-class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(100), nullable = False)
-  price = db.Column(db.Integer, nullable = False)
-  isActive = db.Column(db.Boolean, default=True)
-  text = db.Column(db.Text, nullable = False)
+# class Item(db.Model):
+#   id = db.Column(db.Integer, primary_key=True)
+#   title = db.Column(db.String(100), nullable = False)
+#   price = db.Column(db.Integer, nullable = False)
+#   isActive = db.Column(db.Boolean, default=True)
+#   text = db.Column(db.Text, nullable = False)
 
-# db.create_all()
+# # db.create_all()
 
-def __repr__(self):
-  return self.title
+# def __repr__(self):
+#   return self.title
 
 @app.route('/')
 def index():
@@ -46,4 +46,4 @@ def create():
     return render_template("create.html")
   
 if __name__ =="__main__":
-  app.run(host='0.0.0.0', port=8081)
+  app.run(host='0.0.0.0', port=5050)
